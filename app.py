96ffce433362d833357587ac0b966597ddcbba94
Lemmah <lemayiannakolah@gmail.com>
2017-07-21 18:55:01 +0300

    [new] app file initialize the main function

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from universities.university_info import UniversitySearcher
+
+def main():
+  print("Welcome to the University Searcher.\n")
+  keyword = input("Please enter keyword identifying university: ")
+  country = input("Please enter country (press enter if not sure):")
+  
+
+if __name__ == "__main__":
+   main()
