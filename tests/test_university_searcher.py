2ea34c0dbedfebce9ea8464a94912fb4932e91e5
Lemmah <lemayiannakolah@gmail.com>
2017-07-21 18:47:59 +0300

    [chore] set up some tests

--- tests/test_university_searcher.py
+++ tests/test_university_searcher.py
@@ -5,4 +5,19 @@ class TestUniversitySearcher(unittest.TestCase):
   def setUp(self):
      # simulating an empty response
      # things to test:
+     # TODO:
      # handle errors for responses, look out for empty responses
+     self.university_searcher = UniversitySearcher("multi","kenya")
+     self.search = UniversitySearcher.search()
+  
+  def test_search_instance(self):
+    ''' Assert if instance of search is creatable '''
+    self.assertEqual(isinstance(self.university_searcher, UniversitySearcher), True)
+
+  def test_search_raises_exception_if_response_is_empty(self):
+    ''' Ensure empty responses are explained '''
+    pass
+
+  def test_search_raises_exception_if_response_not_200(self):
+    ''' Assert that search raises exception if response is not OK '''
+    pass
