c828c2928192482aaa5428b3e3e01939f2009aa6
Lemmah <lemayiannakolah@gmail.com>
2017-07-21 18:39:38 +0300

    [chore] create test structure

---
+++ tests/test_university_searcher.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestUniversitySearcher(unittest.TestCase):
+  ''' Testing the university searcher '''
+  def setUp(self):
+     # simulating an empty response
+     # things to test:
+     # handle errors for responses, look out for empty responses
