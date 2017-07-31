6e4d33e3bd4a34319893812f6fe9a71f0c1fc199
Lemmah <lemayiannakolah@gmail.com>
2017-07-31 13:32:12 +0300

    [chore] Enhance Search by Adding Secondary Search

--- universities/university_info.py
+++ universities/university_info.py
@@ -9,11 +9,32 @@ class UniversitySearcher:
     self.search_parameters = {"name": keyword, "country": country}
 
   def search(self):
+    ''' The primary search, just using the names, it's fast '''
     response = requests.get("http://universities.hipolabs.com/search?", params=self.search_parameters)
     if response.status_code != 200:
       raise Exception("There was an error: {}".format(response.status_code))
     found_universities = json.loads(response.text)
     if len(found_universities) == 0:
-      raise Exception("Sorry, that university is not in our database.")
+      print("Primary search did not get any results, trying secondary search.")
+      return self.secondary_search()
+    return found_universities
+
+  def secondary_search(self):
+    ''' A more fine search using the domain names '''
+    prob_domain = self.search_parameters['name']
+    self.search_parameters['name'] = ''
+    response = requests.get("http://universities.hipolabs.com/search?", params=self.search_parameters)
+    if response.status_code != 200:
+        raise Exception("There was an error: {}".format(response.status_code))
+    all_universities = json.loads(response.text)
+    found_universities = []
+    for university in all_universities:
+      for key, value in university.items():
+          if key.lower() == 'domain':
+              probable_nick = value.split('.')[0]
+              if prob_domain in probable_nick:
+                  found_universities.append(university)
+    if len(found_universities) == 0:
+        raise Exception("Sorry, that university is not in our database.")
     return found_universities
 
